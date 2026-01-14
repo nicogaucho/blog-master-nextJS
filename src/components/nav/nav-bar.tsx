b41a2c1608c38e9d800709cf2deeb6434c7a71a0
Nicola Gasparro <nicola.gasp11@gmail.com>
2026-01-14 16:54:44 -0300

    :sparkles: complete-ui

---
+++ src/components/nav/nav-bar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
+export default function Navbar() {
+  return (
+    <header className="sticky top-0 z-50 w-full shadow-sm bg-white">
+      <nav className="mx-auto flex w-full items-center justify-between px-6 py-3">
+        <div className="flex items-center gap-4">
+          <span className="text-lg font-semibold">Next App</span>
+        </div>
+        <div className="flex items-center">
+          <NavigationMenu>
+            <NavigationMenuList className="gap-4">
+              <NavigationMenuItem>
+                <Button asChild variant="outline">
+                  <Link href={"/signin"}>Sign in</Link>
+                </Button>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Button asChild variant="default">
+                  <Link href={"/signup"}>Sign up</Link>
+                </Button>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </nav>
+    </header>
+  );
+}
